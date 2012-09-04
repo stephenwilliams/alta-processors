7015e0fea239b805c33a1e7283d3c9e65d3a60ae
alta189 <me@alta189.com>
2012-09-04 02:32:46 -0400

    Add support for annotations

--- src/main/java/com/alta189/processors/RequiredConstructorsProccessor.java
+++ src/main/java/com/alta189/processors/RequiredConstructorsProccessor.java
@@ -24,6 +24,7 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.MirroredTypeException;
@@ -41,17 +42,35 @@ import com.alta189.annotations.RequiredConstructors;
 import org.kohsuke.MetaInfServices;
 
 @MetaInfServices(Processor.class)
-@SupportedAnnotationTypes("com.alta189.annotations.RequiredConstructors")
+@SupportedAnnotationTypes("*")
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class RequiredConstructorsProccessor extends GenericProcessor {
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 		for (TypeElement type : ElementFilter.typesIn(roundEnv.getElementsAnnotatedWith(RequiredConstructors.class))) {
+			if (type.getKind().isInterface() || type.getKind() == ElementKind.ANNOTATION_TYPE) {
+				continue;
+			}
 			RequiredConstructors requiredConstructors = type.getAnnotation(RequiredConstructors.class);
 			if (requiredConstructors != null) {
 				for (Constructor constructor : requiredConstructors.value()) {
 					if (!hasConstructor(type, constructor)) {
-						error("missin required constructor", type);
+						error("missing required constructor", type);
+					}
+				}
+			}
+		}
+		for (TypeElement annotation : annotations) {
+			RequiredConstructors requiredConstructors = annotation.getAnnotation(RequiredConstructors.class);
+			if (requiredConstructors != null) {
+				for (Constructor constructor : requiredConstructors.value()) {
+					for (TypeElement type : ElementFilter.typesIn(roundEnv.getElementsAnnotatedWith(annotation))) {
+						if (type.getKind().isInterface() || type.getKind() == ElementKind.ANNOTATION_TYPE) {
+							continue;
+						}
+						if (!hasConstructor(type, constructor)) {
+							error("missing required constructor", type);
+						}
 					}
 				}
 			}
